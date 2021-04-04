e190a99537d653f229f99fa88323506ae0f513c5
Ahmad Nashihuddien <ahmad.nashihuddien@ai.astra.co.id>
2021-04-04 16:58:35 +0700

    fix images

--- crowdfunding/flutter_service_worker.js
+++ crowdfunding/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "b5ce7bd9bdfe15c774dc523ebc04a9ec",
-"index.html": "77ae39f622e56bf17986418477b94609",
-"/": "77ae39f622e56bf17986418477b94609",
-"main.dart.js": "7e9424bd92d3085c07494e92e1e4341f",
+"index.html": "c3cf29cd1c5a1c6d3d1b262006857def",
+"/": "c3cf29cd1c5a1c6d3d1b262006857def",
+"main.dart.js": "0d1ce610bfff1455479b02259592e7e8",
 "favicon.png": "22f347997df00585ac2c835a0fa3afa4",
 "icons/Icon-192.png": "22f347997df00585ac2c835a0fa3afa4",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
