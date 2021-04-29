84f13e0cf105ca2370795b103d87ec3f6b6d0eb1
Ahmad Nashihuddien <ahmad.nashihuddien@ai.astra.co.id>
2021-04-29 09:29:45 +0700

    add page home

--- Donasi/flutter_service_worker.js
+++ Donasi/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "b5ce7bd9bdfe15c774dc523ebc04a9ec",
-"index.html": "136a1da1beec45fabae2ac99ac1d91fe",
-"/": "136a1da1beec45fabae2ac99ac1d91fe",
-"main.dart.js": "7d7866deedb4a7bf2c713da1babba3e4",
+"index.html": "774df955ae661a530f485400d06e4d69",
+"/": "774df955ae661a530f485400d06e4d69",
+"main.dart.js": "500c6f0cf6ff49caa9bed2835bf5be1f",
 "favicon.png": "22f347997df00585ac2c835a0fa3afa4",
 "icons/Icon-192.png": "22f347997df00585ac2c835a0fa3afa4",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
