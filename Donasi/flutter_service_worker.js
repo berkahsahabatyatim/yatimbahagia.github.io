dfe16176513ffad9ace9a538419ab9060e2a4dd5
Ahmad Nashihuddien <ahmad.nashihuddien@ai.astra.co.id>
2021-07-06 09:34:33 +0700

    benerin min value qurban

--- Donasi/flutter_service_worker.js
+++ Donasi/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "b5ce7bd9bdfe15c774dc523ebc04a9ec",
-"index.html": "4007e6e31aa65f09f012f75bd0e738a0",
-"/": "4007e6e31aa65f09f012f75bd0e738a0",
-"main.dart.js": "f41c4b1058db6145710d2cb0c7e53483",
+"index.html": "d3a9d97e217e323c740c3c2f81ae6313",
+"/": "d3a9d97e217e323c740c3c2f81ae6313",
+"main.dart.js": "89b9e973938ba2a2cd3b8e143059ee61",
 "favicon.png": "22f347997df00585ac2c835a0fa3afa4",
 "icons/Icon-192.png": "22f347997df00585ac2c835a0fa3afa4",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
