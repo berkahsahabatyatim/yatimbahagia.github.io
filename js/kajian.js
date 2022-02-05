acd39e6950e960fb0d37695b551ec3ec2716f095
Ahmad Nashihuddien <ahmad.n.id@gmail.com>
2022-02-05 09:42:58 +0700

    add page kajian by id

---
+++ js/kajian.js
@@ -0,0 +1,24 @@
+var url_string = window.location.href;
+var url = new URL(url_string);
+var c = url.searchParams.get("id");
+
+new Vue({
+    el: '#app',
+    data() {
+      return {
+          item: null
+      }
+    },
+    mounted() {
+        var url = "https://raw.githubusercontent.com/berkahsahabatyatim/static-data/main/event-details/"
+        url += c;
+        url += ".json";
+        axios
+          .get(url)
+          .then(response => {
+              this.item = response.data
+              document.querySelector('meta[property="og:description"]').setAttribute("content", this.item.title);
+              document.querySelector('meta[property="og:image"]').setAttribute("content", this.item.img);
+          })
+    },
+})
