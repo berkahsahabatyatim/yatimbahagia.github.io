79ae1db378d2a56276dc408094ddf73e42d96ac6
Ahmad Nashihuddien <ahmad.n.id@gmail.com>
2022-02-11 20:48:21 +0700

    Revert "test string escape"
    This reverts commit bea48c92d745a90a1d821bd4bc97855b30677949.

--- js/components.js
+++ js/components.js
@@ -21,7 +21,7 @@ Vue.component('footer-byi', {
         + `                                    <br /><br />`
         + `                                    <div class="mb-1"><a  style="color: #42a5f5;" href="https://wa.me/6281912002001"><img  class="mr-1" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/500px-WhatsApp.svg.png" width="25" height="25"> 0819-1200-2001</a></div>`
         + `                                    <a style="color: #42a5f5;" class="mb-2 d-block" href="mailto:yayasanberkahyatimindonesia@gmail.com"><img  class="mr-1" src="assets/img/gmail.png" height="20"> yayasanberkahyatimindonesia@gmail.com</a>`
-        + `                                    <a style="color: #42a5f5;" href="\${baseUrl}/toc.html">Syarat dan Ketentuan</a>`
+        + `                                    <a style="color: #42a5f5;" href="${baseUrl}/toc.html">Syarat dan Ketentuan</a>`
         + `                                 </div>`
         + `                              </aside>`
         + `                           </div>`
@@ -41,7 +41,7 @@ Vue.component('footer-byi', {
         + `                                 </div>`
         + `                              </aside>`
         + `                           </div>`
-        + `                           <img src="\${baseUrl}/assets/img/logobyi-white.jpeg" class="img-fluid col-md-4 mb-3 " alt="Responsive image" `
+        + `                           <img src="${baseUrl}/assets/img/logobyi-white.jpeg" class="img-fluid col-md-4 mb-3 " alt="Responsive image" `
         + `                           `
         + `                           >`
         + `                        </div>`
@@ -59,55 +59,55 @@ Vue.component('footer-byi', {
 Vue.component('navbar-byi', {
     template: `<nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">`
         + `    <div class="container">`
-        + `        <a class="navbar-brand js-scroll-trigger" href=\${baseUrl}>`
-        + `            <img class="navbar-brand js-scroll-trigger" src="\${baseUrl}/assets/img/logobyi.png" width="50" height="53"/>`
+        + `        <a class="navbar-brand js-scroll-trigger" href=${baseUrl}>`
+        + `            <img class="navbar-brand js-scroll-trigger" src="${baseUrl}/assets/img/logobyi.png" width="50" height="53"/>`
         + `        </a>`
         + `        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>`
         + `        <div class="collapse navbar-collapse" id="navbarResponsive">`
         + `            <ul class="navbar-nav ml-auto my-2 my-lg-0">`
-        + `                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="\${baseUrl}">Home</a></li>`
+        + `                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="${baseUrl}">Home</a></li>`
         + `                <li class="dropdown mr-3">`
         + `                    <div class="nav-item">`
         + `                        <a class="nav-link js-scroll-trigger">Tentang Kami</a>`
         + `                        <div class="dropdown-content bg-dark">`
-        + `                            <a href="\${baseUrl}/about/about.html#profil">Profil Yayasan BYI</a>`
-        + `                            <a href="\${baseUrl}/about/visimisi.html#profil">Visi Misi</a>`
-        + `                            <a href="\${baseUrl}/about/pengurus.html#legal">Kepengurusan</a>`
-        + `                            <a href="\${baseUrl}/about/legalitas.html#legal">Legalitas</a>`
+        + `                            <a href="${baseUrl}/about/about.html#profil">Profil Yayasan BYI</a>`
+        + `                            <a href="${baseUrl}/about/visimisi.html#profil">Visi Misi</a>`
+        + `                            <a href="${baseUrl}/about/pengurus.html#legal">Kepengurusan</a>`
+        + `                            <a href="${baseUrl}/about/legalitas.html#legal">Legalitas</a>`
         + `                        </div>`
         + `                    </div>`
         + `                </li>`
-        + `                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="\${baseUrl}/album.html#album">Dokumentasi</a></li>`
+        + `                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="${baseUrl}/album.html#album">Dokumentasi</a></li>`
         + `                <li class="dropdown mr-3">`
         + `                    <div class="nav-item">`
         + `                        <a class="nav-link js-scroll-trigger">Program Kerja</a>`
         + `                        <div class="dropdown-content bg-dark">`
-        + `                            <a href="\${baseUrl}/program/pondok.html#services">Pendidikan dan Pembinaan Pondok Tahfidz Yatim & Dhuafa</a>`
-        + `                            <a href="\${baseUrl}/program/pondok.html#services">Pembangunan dan Pembinaan Kampung Tauhid</a>`
-        + `                            <a href="\${baseUrl}/program/beasiswa.html">Beasiswa Pendidikan Formal untuk Yatim & Dhuafa</a>`
-        + `                            <a href="\${baseUrl}/program/lahan.html">Pembebasan Lahan untuk Pondok Tahfidz Yatim & Dhuafa</a>`
-        + `                            <a href="\${baseUrl}/program/masjid.html">Pembangunan Masjid</a>`
-        + `                            <a href="\${baseUrl}/program/pondok.html#services">Pembangunan Pondok Tahfidz Yatim & Dhuafa</a>`
-        + `                            <a href="\${baseUrl}/program/ekonomi.html">Pemberdayaan Ekonomi Masyarakat</a>`
-        + `                            <a href="\${baseUrl}/program/santripreneur.html">Kafalah Du\'at</a>`
-        + `                            <a href="\${baseUrl}/program/bantuan-bencana.html">Bantuan Kemanusiaan</a>`
-        + `                            <a href="\${baseUrl}/program/santripreneur.html">Pembinaan Pemuda Berdaya</a>`
-        + `                            <a href="\${baseUrl}/program/santripreneur.html">Pengelolaan Wakaf Produktif</a>`
+        + `                            <a href="${baseUrl}/program/pondok.html#services">Pendidikan dan Pembinaan Pondok Tahfidz Yatim & Dhuafa</a>`
+        + `                            <a href="${baseUrl}/program/pondok.html#services">Pembangunan dan Pembinaan Kampung Tauhid</a>`
+        + `                            <a href="${baseUrl}/program/beasiswa.html">Beasiswa Pendidikan Formal untuk Yatim & Dhuafa</a>`
+        + `                            <a href="${baseUrl}/program/lahan.html">Pembebasan Lahan untuk Pondok Tahfidz Yatim & Dhuafa</a>`
+        + `                            <a href="${baseUrl}/program/masjid.html">Pembangunan Masjid</a>`
+        + `                            <a href="${baseUrl}/program/pondok.html#services">Pembangunan Pondok Tahfidz Yatim & Dhuafa</a>`
+        + `                            <a href="${baseUrl}/program/ekonomi.html">Pemberdayaan Ekonomi Masyarakat</a>`
+        + `                            <a href="${baseUrl}/program/santripreneur.html">Kafalah Du\'at</a>`
+        + `                            <a href="${baseUrl}/program/bantuan-bencana.html">Bantuan Kemanusiaan</a>`
+        + `                            <a href="${baseUrl}/program/santripreneur.html">Pembinaan Pemuda Berdaya</a>`
+        + `                            <a href="${baseUrl}/program/santripreneur.html">Pengelolaan Wakaf Produktif</a>`
         + `                        </div>`
         + `                    </div>`
         + `                </li>`
-        + `                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="\${baseUrl}/donasi.html#donate">Donasi</a></li>`
-        + `                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="\${baseUrl}/kegiatan-byi.html#activity">Kegiatan</a></li>`
-        + `                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="\${baseUrl}#contact">Hubungi Kami</a></li>`
+        + `                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="${baseUrl}/donasi.html#donate">Donasi</a></li>`
+        + `                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="${baseUrl}/kegiatan-byi.html#activity">Kegiatan</a></li>`
+        + `                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="${baseUrl}#contact">Hubungi Kami</a></li>`
         + `                <li class="dropdown">`
         + `                    <div class="nav-item">`
         + `                        <a class="nav-link js-scroll-trigger">Artikel</a>`
         + `                        <div class="dropdown-content bg-dark">`
-        + `                            <a href="\${baseUrl}/artikel/manfaat-sedekah.html#main-content">Manfaat Sedekah</a>`
-        + `                            <a href="\${baseUrl}/artikel/wakaf.html#main-content">Keutamaan, Rukun & Syarat Wakaf</a>`
-        + `                            <a href="\${baseUrl}/artikel/zakat.html#main-content">Syarat Wajib Zakat & Cara Menunaikannya</a>`
-        + `                            <a href="\${baseUrl}/artikel/penerima-zakat.html#main-content">8 Golongan yang Berhak Menerima Zakat</a>`
-        + `                            <a href="\${baseUrl}/artikel/qurban.html#main-content">13 Tips Memilih Hewan untuk Kurban</a>`
+        + `                            <a href="${baseUrl}/artikel/manfaat-sedekah.html#main-content">Manfaat Sedekah</a>`
+        + `                            <a href="${baseUrl}/artikel/wakaf.html#main-content">Keutamaan, Rukun & Syarat Wakaf</a>`
+        + `                            <a href="${baseUrl}/artikel/zakat.html#main-content">Syarat Wajib Zakat & Cara Menunaikannya</a>`
+        + `                            <a href="${baseUrl}/artikel/penerima-zakat.html#main-content">8 Golongan yang Berhak Menerima Zakat</a>`
+        + `                            <a href="${baseUrl}/artikel/qurban.html#main-content">13 Tips Memilih Hewan untuk Kurban</a>`
         + `                        </div>`
         + `                    </div>`
         + `                </li>`
